67dbeffb90ea03f0ee5fc5b37fd8889bbd306b26
Joe <reed.joseph13@gmail.com>
2019-04-22 12:18:13 -0500

    Updates

---
+++ precache-manifest.ed1a405dbf6290381e4d6b04f3dba4f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3eb011215411f1d5a5fb",
+    "url": "/react-complete-guide/static/js/runtime~main.934f712a.js"
+  },
+  {
+    "revision": "ed323dc98b3494b3f645",
+    "url": "/react-complete-guide/static/js/main.c0b88c01.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-complete-guide/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ed323dc98b3494b3f645",
+    "url": "/react-complete-guide/static/css/main.bb753414.chunk.css"
+  },
+  {
+    "revision": "aa7bbd14060f19c875a6f6a1bf5ea504",
+    "url": "/react-complete-guide/index.html"
+  }
+];
