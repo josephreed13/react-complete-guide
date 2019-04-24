6c99191132cb773186ccadd4d5f154e6d037b9d0
Joe <reed.joseph13@gmail.com>
2019-04-24 17:26:50 -0500

    66. Adding and Using Radium

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,6 @@
 import React from 'react';
-
 import './Person.css';
-
+import Radium from 'radium';
 const person = (props) => {
 	return (
 		<div className="Person">
@@ -12,4 +11,4 @@ const person = (props) => {
 	)
 };
 
-export default person;
+export default Radium(person);
