f9d04939f94ca2c7b541ffee653978cb31b78dcb
Joe <reed.joseph13@gmail.com>
2019-06-04 21:26:57 -0500

    split up app into components -- better structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 // JS object which gives you access to a string version of your css styles. The CSS style was adjusted to be unique
 // this is the only place where these styles can have an effect. (CSS Modules)
 import classes from './App.css';
-import Person from './Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
 	state = {
@@ -55,39 +56,26 @@ class App extends Component {
 	render() {
 
 		let persons = null;
-		let btnClass = '';
 
 		if (this.state.showPersons) {
 			persons = (
-				<div>
-					{this.state.persons.map((person, index) => {
-						return <Person 
-								click={() => this.deletePersonsHandler(index)}
-								name={person.name} 
-								age={person.age}
-								key={person.id}
-								changed={(event) => this.nameChangedHandler(event, person.id)}/>
-					})}
-		        </div>
+					<Persons 
+						persons={this.state.persons}
+						clicked={this.deletePersonsHandler}
+						changed={this.nameChangedHandler}
+					/>
 			);
 
-			btnClass = classes.Red;
 
 		}
 
-		const assignedClasses = [];
-		if(this.state.persons.length <= 2) {
-			assignedClasses.push(classes.red); // classes = ['red']
-		}
-		if(this.state.persons.length <= 1) {
-			assignedClasses.push(classes.bold); // classes = ['red','bold']
-		}
 	    return (
 		      	<div className={classes.App}>
-			        <h1>Hi, I'm a React App</h1>
-			        <p className={assignedClasses.join(' ')}>This is really working!</p>
-			        <button className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons</button>
-			        
+			        <Cockpit 
+			        showPersons={this.state.showPersons}
+			        persons={this.state.persons}
+			        toggle={this.togglePersonsHandler}
+			        />
 			        {persons}
 
 			    </div>
